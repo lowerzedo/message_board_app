28cc094261bd03d65ae949b6554f8105531c74c3
lowerzedo <tp052475@mail.apu.edu.my>
2024-05-14 10:42:43 +0800

    feat: home page to view posts

--- posts/models.py
+++ posts/models.py
@@ -3,5 +3,6 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     text = models.TextField()
+    # human readable, first 50 characters
     def __str__(self):
         return self.text[:50]
